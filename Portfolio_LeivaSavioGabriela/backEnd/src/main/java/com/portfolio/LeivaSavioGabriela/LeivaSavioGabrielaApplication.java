e597b84c56221a6356a80c092b3731d6c4d01336
GabyLS <91224666+GabyLS@users.noreply.github.com>
2022-08-02 15:50:01 -0300

    Cree el backend

---
+++ Portfolio_LeivaSavioGabriela/backEnd/src/main/java/com/portfolio/LeivaSavioGabriela/LeivaSavioGabrielaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.LeivaSavioGabriela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeivaSavioGabrielaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeivaSavioGabrielaApplication.class, args);
+	}
+
+}
---
+++ Portfolio_LeivaSavioGabriela/backEnd/src/test/java/com/portfolio/LeivaSavioGabriela/LeivaSavioGabrielaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.LeivaSavioGabriela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeivaSavioGabrielaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

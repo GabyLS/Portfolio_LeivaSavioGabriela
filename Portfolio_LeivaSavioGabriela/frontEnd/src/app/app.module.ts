554f161f19e0e1bc07e7afdcb0b3140d225dfa18
GabyLS <91224666+GabyLS@users.noreply.github.com>
2022-08-17 17:24:14 -0300

    Configuré frontend y backend

--- Portfolio_LeivaSavioGabriela/frontEnd/src/app/app.module.ts
+++ Portfolio_LeivaSavioGabriela/frontEnd/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -17,6 +17,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,13 +33,15 @@ import { LoginComponent } from './components/login/login.component';
     ProyectosComponent,
     FooterComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    
 
   ],
   providers: [],
